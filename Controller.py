ea39b02a6f243bc225eedd64c9aa1aa814841cb1
HonWiCong <102761891@students.swinburne.edu.my>
2024-05-24 01:06:04 +0800

    .

--- Controller.py
+++ Controller.py
@@ -12,7 +12,7 @@ class Controller:
         self.private_switch_on_light = private_switch_on_light
         self.private_switch_off_light = private_switch_off_light
         
-        self.private_message = private_message
+        # self.private_message = private_message
         self.private_code = private_code
 
         self.public_always_open_gate = public_always_open_gate
@@ -22,7 +22,7 @@ class Controller:
         self.public_switch_on_light = public_switch_on_light
         self.public_switch_off_light = public_switch_off_light
         
-        self.public_message = public_message
+        # self.public_message = public_message
         self.public_code = public_code
 
     # default constructor
@@ -34,7 +34,7 @@ class Controller:
         self.private_switch_on_light = 0
         self.private_switch_off_light = 0
         
-        self.private_message = ""
+        # self.private_message = ""
         self.private_code = 0
 
         self.public_always_open_gate = 0
@@ -44,7 +44,7 @@ class Controller:
         self.public_switch_on_light = 0
         self.public_switch_off_light = 0
         
-        self.public_message = ""
+        # self.public_message = ""
         self.public_code = 0
 
     def toJson(self):
