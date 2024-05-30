c930e30126430c2d2a9243061a4228058ccf58ae
HonWiCong <102761891@students.swinburne.edu.my>
2024-05-30 19:08:46 +0800

    .

--- mqtt/Cloud_AWS_MQTT.py
+++ mqtt/Cloud_AWS_MQTT.py
@@ -18,9 +18,9 @@ database = mysql.connector.connect(
 )
 
 endpoint = "a27eliy2xg4c5e-ats.iot.us-east-1.amazonaws.com"
-cert_filepath = r"\home\pi\PrivateParking\mqtt\44bdbb017ed61e3180473d7562a7219625694010abfe0315ab96632a7fe8402b-certificate.pem.crt"
-pri_key_filepath = r"\home\pi\PrivateParking\mqtt\44bdbb017ed61e3180473d7562a7219625694010abfe0315ab96632a7fe8402b-private.pem.key"
-ca_filepath = r"\home\pi\PrivateParking\mqtt\AmazonRootCA1.pem"
+cert_filepath = r"home\pi\PrivateParking\mqtt\44bdbb017ed61e3180473d7562a7219625694010abfe0315ab96632a7fe8402b-certificate.pem.crt"
+pri_key_filepath = r"home\pi\PrivateParking\mqtt\44bdbb017ed61e3180473d7562a7219625694010abfe0315ab96632a7fe8402b-private.pem.key"
+ca_filepath = r"home\pi\PrivateParking\mqtt\AmazonRootCA1.pem"
 client_id = "ParkingSlot"
 
 # AWS IoT MQTT Client Setup
